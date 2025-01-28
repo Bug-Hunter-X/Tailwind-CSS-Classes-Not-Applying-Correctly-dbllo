7c33e7e44fd72ba77cfbf9145160ee544492bd25
BugHunter-X <askzenai@gmail.com>
2025-01-28 07:02:40 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Some JavaScript code using Tailwind CSS classes
+div className="bg-gray-200 p-4 rounded shadow">
+  <p className="text-xl font-bold">This is some text.</p>
+</div>
+```
